934a8043ade2027f2cc49eed0d0a33d05aa309a6
Daniel <danielubt@hotmail.com>
2020-04-05 22:36:52 -0300

    adcionando o diretorio front

---
+++ front/script.js
@@ -0,0 +1,178 @@
+//Excluir usuario
+$(document).ready(function () {
+    $("#excluir").click(function () {
+      var matricula = $("#matriculaD").val();
+      var excluir = "http://localhost:3000/usuarios/excluir";
+      $.ajax({
+        type: "post",
+        url: excluir,
+        dataType: "json",
+        data: { "matricula": matricula},
+        success: function (data) {
+          console.log(data);
+          alert("Usuario excluido com sucesso!")
+        },
+        error: function (erro) {
+          console.log(erro)
+          alert(erro)
+  
+        }
+      })
+    })
+  })
+
+
+//Editar Usuario
+$(document).ready(function () {
+    $("#atualizar").click(function () {
+      var matricula = $("#matE").val();
+      var nome =   $("#nomeE").val();
+      var email =   $("#emailE").val();
+      var id_curso =   $("#id_cursoE").val();
+      var ativo =   $("#ativoE").val();
+      var atualizar = "http://localhost:3000/usuarios/atualizar";
+                   
+      $.ajax({
+        type: "POST",
+        url: atualizar,
+        dataType: "json",
+        data: { 
+            "matricula": matricula,
+            "nome": nome,
+            "email": email,
+            "id_curso": id_curso,
+            "ativo": ativo},
+        success: function (data) {
+          console.log(data);
+          alert("Usuario atualizado com sucesso")
+        },
+        error: function (erro) {
+          console.log(erro)
+  
+        }
+      })
+    })
+  })
+  
+
+//Fazer Login
+$(document).ready(function () {
+    $("#acessar").click(function () {
+      var matricula = $("#matriculaA").val();
+      var senha =   $("#senhaA").val();
+      var login = "http://localhost:3000/usuarios/login";
+      $.ajax({
+        type: "POST",
+        url: login,
+        dataType: "json",
+        data: { "matricula": matricula,"senha": senha},
+        success: function (data) {
+          console.log(data);
+          $("#token").val(data.token)
+          alert("Acesso Permitido")
+        },
+        error: function (erro) {
+          console.log(erro)
+          alert(erro)
+  
+        }
+      })
+    })
+  })
+  
+
+//localizar produto
+ $(document).ready(function(){
+    $("#localizar").click(function(){
+      var mat= $("#mat").val();
+      var urlLocalizaUsuario = "http://localhost:3000/usuarios/localizar/"+ mat;
+      $.ajax({
+        url : urlLocalizaUsuario,
+        type : "GET",
+        dataType : "json",
+        success : function(data){
+         $("#matriculaL").val(data.usuario.matricula);
+         $("#nomeL").val(data.usuario.nome);
+         $("#emailL").val(data.usuario.email); 
+         $("#id_cursoL").val(data.usuario.id_curso); 
+         $("#ativoL").val(data.usuario.ativo);
+         console.log(data) 
+        },
+        error: function (erro) {
+          console.log(erro)
+        }
+      })
+    })
+  })
+
+//Cadastrar Usuario
+$(document).ready(function () {
+    $("#cadastrar").click(function () {
+      var matricula = $("#matriculaC").val();
+      var nome =   $("#nomeC").val();
+      var email =   $("#emailC").val();
+      var senha =   $("#senhaC").val();
+      var id_curso =   $("#id_cursoC").val();
+      var ativo =   $("#ativoC").val();
+      var cadastrar = "http://localhost:3000/usuarios/cadastrar";
+      $.ajax({
+        type: "POST",
+        url: cadastrar,
+        dataType: "json",
+        data: { 
+            "matricula": matricula, "nome": nome,
+            "email": email, "senha": senha,
+            "id_curso": id_curso, "ativo": ativo},
+        success: function (data) {
+          console.log(data);
+          alert("Usuario criado com sucesso")
+        },
+        error: function (erro) {
+          console.log(erro)
+  
+        }
+      })
+    })
+  })
+  
+  
+  //lista os produtos
+$(document).ready(function(){
+  $("#listar").click(function(){
+    $.each({ name: "John", lang: "JS" }, function( k, v ) {
+      alert( "Key: " + k + ", Value: " + v );
+    });})})
+  $(document).ready(function () {
+    $("#listara").click(function () {
+      var listar = "http://localhost:3000/usuarios/listar";
+      var elementoPai = $(".pai").clone();
+      $(".pai").remove();
+      $.ajax({
+        url: listar,
+        type: "GET",
+        dataType: "json",
+        success: function (data) {
+          $.each(data
+            , function(index, item){
+
+            alert(index+" : "+item)
+          // elementoPai.find(".titulosPostagens").html(item.Usuarios.matricula);
+          // elementoPai.find(".p").html(item.Usuarios.nome);
+          // elementoPai.find(".v").html(item.Usuarios.email);
+          // elementoPai.find(".dataTermino").html(item.Usuarios.id_curso);
+          // elementoPai.find(".le").html(item.Usuarios.ativo);
+          // $('.conteudo').append(elementoPai.clone());
+          })
+          console.log(data);
+        },
+        error: function (erro) {
+          console.log(erro)
+        }
+      });
+    })
+  })
+  
+ 
+  
+  
+  
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./app')
 
 app.listen(3000, function(){
-    console.log("Servidor HTTP criado com sucesso!")
+    console.log("Servidor HTTP criado com sucesso porta 3000! no nodejs")
 });
